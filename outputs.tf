1827b9483495c331c9579d5358eb4487b230541f
Mencery <plekhanov1090@gmail.com>
2025-07-15 20:26:33 +0000

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@
 ##################################################################################
 
 output "vpc_id" {
-  value = module.main.vpc_id
+  value       = module.main.vpc_id
   description = "VPC_ID"
 }
 
 output "public_subnets" {
-  value = module.main.public_subnets
+  value       = module.main.public_subnets
   description = "List of public subnets"
 }
